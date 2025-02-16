662eaa6e62b2d4d35ce68e61b23912aae3da865d
cmdg01 <courtneymlambo394@gmail.com>
2025-02-16 05:51:17 +0200

    Disable IdeHelper in production

--- config/app.php
+++ config/app.php
@@ -179,7 +179,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Mariuzzo\LaravelJsLocalization\LaravelJsLocalizationServiceProvider::class,
         Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
