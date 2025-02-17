09d06d940ee0174f6df4acc6e3c924337f0d7d69
cmdg01 <courtneymlambo394@gmail.com>
2025-02-17 08:34:55 +0200

    Fix ide-helper loading in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
         $this->app->singleton('localization.js', function ($app) {
             $app = $this->app;
             $laravelMajorVersion = (int) $app::VERSION;
